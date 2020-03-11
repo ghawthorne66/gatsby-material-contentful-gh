ca2f84d99366e212ee03dd0e55debd84173389d4
Greg Hawthorne <ghawthorne@me.com>
2020-03-11 08:17:53 -0700

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,8 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken:process.env.CONTENTFUL_ACCESS_TOKEN,
-        environment: process.env.CONTENTFUL_ENV,
+        // environment: process.env.CONTENTFUL_ENV,
+        environment: "master",
       }
     },
   ],
